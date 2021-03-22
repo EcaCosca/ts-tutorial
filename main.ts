fe0c3cb88d91e4f36d0317ce95ed3aa83c79ae92
EcaCosca <enriquecoscarelli@gmail.com>
2021-03-22 17:45:04 +0100

    Declared different kinds of variables and introduced the use of enum

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i)
-}
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
 
-doSomething();
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3};
+let backgroundColor = Color.Red;
